10fc204e53c1b2ef95d02db0d37eeccc4264a2e5
ThomaSpyrou <spyroutommy@gmail.com>
2021-04-17 00:05:52 +0300

    Added session policy

--- src/main/java/com/appsdev/app/ws/appdevws/security/WebSecurity.java
+++ src/main/java/com/appsdev/app/ws/appdevws/security/WebSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -28,7 +29,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, SecurityConstants.SING_UP_URL)
                 .permitAll()
                 .anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
-                .addFilter(new AuthorizationFilter(authenticationManager()));
+                .addFilter(new AuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); //in order not to create http session and cashed cookies
     }
 
     @Override
