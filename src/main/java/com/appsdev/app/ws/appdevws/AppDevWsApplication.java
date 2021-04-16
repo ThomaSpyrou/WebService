e103b68b8c27204b68a6fdb58573179b17985f00
ThomaSpyrou <spyroutommy@gmail.com>
2021-04-17 00:48:41 +0300

    Fixed-added bean in spring application for AppProperties

--- src/main/java/com/appsdev/app/ws/appdevws/AppDevWsApplication.java
+++ src/main/java/com/appsdev/app/ws/appdevws/AppDevWsApplication.java
@@ -1,5 +1,6 @@
 package com.appsdev.app.ws.appdevws;
 
+import com.appsdev.app.ws.appdevws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,9 @@ public class AppDevWsApplication {
 
 	@Bean
 	public SpringApplicationContext springApplicationContext(){return new SpringApplicationContext();}
+
+	@Bean(name = "AppProperties")
+	public AppProperties appProperties(){return new AppProperties();}
 
 
 }
