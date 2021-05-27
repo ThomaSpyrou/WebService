86a884b621b2ed0974e18a7190878a40eb08f790
ThomaSpyrou <spyroutommy@gmail.com>
2021-05-27 11:14:58 +0300

    Added sql queries for custom operations, and tested

--- src/main/java/com/appsdev/app/ws/appdevws/io/repositories/UserRepository.java
+++ src/main/java/com/appsdev/app/ws/appdevws/io/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.appsdev.app.ws.appdevws.io.repositories;
 
 import com.appsdev.app.ws.appdevws.io.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
@@ -11,4 +17,30 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
     UserEntity findUserEntitiesByEmail(String email);
     UserEntity findUserEntityByUserId(String userId);
     UserEntity findUserEntityByEmailVerificationToken(String token);
+
+    @Query(value = "select * from users where email_verification_status = 'true'",
+            nativeQuery = true,
+            countQuery = "select count(*) from users where email_verification_status = 'true'")
+        //since pagination is used need to provide countQuery
+    Page<UserEntity> findAllUsersWithConfirmedEmail(Pageable pageableRequest);
+
+    @Query(value = "select * from users where last_name = :lastName",
+            nativeQuery = true)
+    List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
+
+    @Query(value = "select * from users where first_name like %:keyword% or first_name like %:keyword%",
+            nativeQuery = true)
+    List<UserEntity> findUserByKeyword(@Param("keyword") String lastName);
+
+    //select specific columns
+    @Query(value = "select first_name, last_name from users where first_name like %:keyword% or first_name like %:keyword%",
+            nativeQuery = true)
+    List<Object[]> findUserByKeyword_v2(@Param("keyword") String lastName);
+
+    @Transactional
+    @Modifying //modifying is used also in delete operation
+    @Query(value= "update users set email_verification_status = :emailVerificationStatus where user_id = :userID",
+            nativeQuery = true)
+    void updateUserEmailVerificationStatus(@Param("emailVerificationStatus") boolean emailVerificationStatus, @Param("userID") String userID);
+
 }
---
+++ src/test/java/com/appsdev/app/ws/appdevws/UserRepositoryTest.java
@@ -0,0 +1,99 @@
+package com.appsdev.app.ws.appdevws;
+
+import com.appsdev.app.ws.appdevws.io.entity.UserEntity;
+import com.appsdev.app.ws.appdevws.io.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest //an integration test since we need to select/update the DB
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    UserEntity userEntity;
+    String userId = "fef32";
+    String encryptedPassword = "32rfw3";
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setFirstName("thomas");
+        userEntity.setLastName("sth");
+        userEntity.setUserId(userId);
+        userEntity.setEncryptedPassword(encryptedPassword);
+        userEntity.setEmailVerificationToken("sdfgh");
+        userEntity.setEmail("test@test.com");
+        userEntity.setEmailVerificationStatus(true);
+        userRepository.save(userEntity);
+    }
+
+    @Test
+    final void testGetVerifiedUsers() {
+        Pageable pageableRequest = PageRequest.of(0, 2);
+        Page<UserEntity> pages = userRepository.findAllUsersWithConfirmedEmail(pageableRequest);
+        List<UserEntity> userEntityList = pages.getContent();
+
+        assertEquals(1, userEntityList.size());
+        assertNotNull(pages);
+    }
+
+    @Test
+    final void testFindUserByLastName(){
+        String lastName = "sth";
+        List<UserEntity> users = userRepository.findUserByLastName(lastName);
+        assertNotNull(users);
+
+        UserEntity user = users.get(0);
+        assertEquals(user.getLastName(), lastName);
+    }
+
+    @Test
+    final void testFindUserByKeyword(){
+        String keyword = "th";
+        List<UserEntity> users = userRepository.findUserByKeyword(keyword);
+        assertNotNull(users);
+    }
+
+    @Test
+    final void testFindUserByKeyword_v2(){
+        String keyword = "th";
+        List<Object[]> users = userRepository.findUserByKeyword_v2(keyword);
+        assertNotNull(users);
+
+        Object[] user = users.get(0);
+        String user_first_name = String.valueOf(user[0]);
+        String user_last_name = String.valueOf(user[1]);
+
+        System.out.println(user_first_name + " "+ user_last_name);
+
+        assertNotNull(user_first_name);
+        assertNotNull(user_last_name);
+    }
+
+    @Test
+    final void testUpdateUserEmailVerificationStatus(){
+        boolean emailVerificationStatus = false;
+        userRepository.updateUserEmailVerificationStatus(emailVerificationStatus, userId);
+        UserEntity storedDetailed = userRepository.findUserEntityByUserId(userId);
+
+        boolean storedEmailVerificationStatus = storedDetailed.getEmailVerificationStatus();
+
+        assertEquals(storedEmailVerificationStatus, emailVerificationStatus);
+    }
+}
+
